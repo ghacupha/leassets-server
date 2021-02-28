94a81dda32991d5376dc65478a0073835cafe42f
ghacupha <mailnjeru@gmail.com>
2021-02-28 11:02:21 +0300

    Initial version of leassetsServer generated by JHipster-6.10.5

---
+++ src/main/java/io/github/leassets/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package io.github.leassets.service.dto;
---
+++ src/main/java/io/github/leassets/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package io.github.leassets.web.rest.vm;
