b51c8adab6175125c914d5c4fc2d0327f598c577
Edwin Njeru <mailnjeru@gmail.com>
2021-04-07 18:37:32 +0300

    Configure custom batch configurer with XStream deserializer

--- src/main/java/io/github/leassets/internal/batch/framework/BatchConfigs.java
+++ src/main/java/io/github/leassets/internal/batch/framework/BatchConfigs.java
@@ -1,12 +1,20 @@
 package io.github.leassets.internal.batch.framework;
 
 import io.github.leassets.internal.batch.PersistenceJobListener;
+import javax.sql.DataSource;
 import org.springframework.batch.core.JobExecutionListener;
+import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.dao.XStreamExecutionContextStringSerializer;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * Contains general configurations for batch related entities and objects
@@ -25,5 +33,20 @@ public class BatchConfigs {
         @Value("#{jobParameters['startUpTime']}") long startUpTime
     ) {
         return new PersistenceJobListener(fileId, startUpTime);
+    }
+
+    @Bean
+    BatchConfigurer configurer(@Qualifier("dataSource") DataSource dataSource, PlatformTransactionManager transactionManager) {
+        return new DefaultBatchConfigurer(dataSource) {
+            @Override
+            protected JobRepository createJobRepository() throws Exception {
+                JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+                factory.setDataSource(dataSource);
+                factory.setTransactionManager(transactionManager);
+                factory.setSerializer(new XStreamExecutionContextStringSerializer());
+                factory.afterPropertiesSet();
+                return factory.getObject();
+            }
+        };
     }
 }
